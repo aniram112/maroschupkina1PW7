6c85c3b034281d07b384345c9030c9d6606acc81
aniram112 <masyakmarina@gmail.com>
2022-01-23 23:06:57 +0300

    3 points

--- maroschupkina1PW7/Controllers/ViewController.swift
+++ maroschupkina1PW7/Controllers/ViewController.swift
@@ -10,11 +10,12 @@ import CoreLocation
 import MapKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .darkGray
         configureUI()
+        setupHideKeyboardOnTap()
         // Do any additional setup after loading the view.
     }
     
@@ -36,10 +37,10 @@ class ViewController: UIViewController {
         let stack = UIStackView()
         stack.axis = .horizontal
         stack.alignment = .center
-        stack.spacing = 15
-        stack.backgroundColor = UIColor.systemGray6
+        stack.spacing = 30
+        //stack.backgroundColor = UIColor.systemGray6
         stack.distribution = .equalCentering
-        stack.layoutMargins = UIEdgeInsets(top: 5, left: 15, bottom: 5, right: 15)
+        stack.layoutMargins = UIEdgeInsets(top: 5, left: 5, bottom: 5, right: 5)
         stack.isLayoutMarginsRelativeArrangement = true
         stack.layer.cornerRadius = 10
         stack.translatesAutoresizingMaskIntoConstraints = false
@@ -48,10 +49,10 @@ class ViewController: UIViewController {
     
     let startLocation: UITextField = {
         let control = UITextField()
-        control.backgroundColor = UIColor.lightGray
+        control.backgroundColor = .lightGray.withAlphaComponent(0.4)
         control.textColor = UIColor.black
         control.placeholder = "From"
-        control.layer.cornerRadius = 2
+        control.layer.cornerRadius = 20
         control.clipsToBounds = false
         control.font = UIFont.systemFont(ofSize: 15)
         control.borderStyle = UITextField.BorderStyle.roundedRect
@@ -67,10 +68,10 @@ class ViewController: UIViewController {
     
     let finishLocation: UITextField = {
         let control = UITextField()
-        control.backgroundColor = UIColor.lightGray
+        control.backgroundColor = .lightGray.withAlphaComponent(0.4)
         control.textColor = UIColor.black
         control.placeholder = "To"
-        control.layer.cornerRadius = 2
+        control.layer.cornerRadius = 20
         control.clipsToBounds = false
         control.font = UIFont.systemFont(ofSize: 15)
         control.borderStyle = UITextField.BorderStyle.roundedRect
@@ -85,13 +86,13 @@ class ViewController: UIViewController {
     }()
     
     let goButton : RoundButtonView = {
-        let goButton = RoundButtonView(color: .orange.withAlphaComponent(0.8), text: "Go")
+        let goButton = RoundButtonView(color: .lightGray.withAlphaComponent(0.4), text: "Go")
         goButton.addTarget(self, action: #selector(goButtonWasPressed), for: .touchDown)
         return goButton
     }()
     
     let clearButton : RoundButtonView = {
-        let clearButton = RoundButtonView(color: .orange.withAlphaComponent(0.8), text: "Clear")
+        let clearButton = RoundButtonView(color: .lightGray.withAlphaComponent(0.4), text: "Clear")
         clearButton.addTarget(self, action: #selector(clearButtonWasPressed), for: .touchDown)
         return clearButton
     }()
@@ -106,7 +107,7 @@ class ViewController: UIViewController {
     @objc func goButtonWasPressed(_ sender: UIButton) {
         sender.isEnabled = false
     }
-
+    
     private func configureUI() {
         view.addSubview(map)
         map.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
@@ -114,7 +115,69 @@ class ViewController: UIViewController {
         map.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
         map.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
         
+        buttonsStack.addArrangedSubview(goButton)
+        buttonsStack.addArrangedSubview(clearButton)
+        view.addSubview(buttonsStack)
+        buttonsStack.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        buttonsStack.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        buttonsStack.heightAnchor.constraint(equalToConstant: UIScreen.main.bounds.height / 10).isActive = true
+        buttonsStack.layer.masksToBounds = true
+        
+        let textStack = UIStackView()
+        textStack.axis = .vertical
+        view.addSubview(textStack)
+        textStack.spacing = 10
+        textStack.translatesAutoresizingMaskIntoConstraints = false
+        textStack.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
+        textStack.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
+        textStack.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10
+        ).isActive = true
+        textStack.addArrangedSubview(startLocation)
+        textStack.addArrangedSubview(finishLocation)
+        startLocation.delegate = self
+        finishLocation.delegate = self
+        
     }
+    
+    
+}
 
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        if startLocation.text != "" && finishLocation.text != "" {
+            goButtonWasPressed(goButton)
+        }
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if startLocation.text != "" && finishLocation.text != "" {
+            goButton.isEnabled = true
+            clearButton.isEnabled = true
+        }
+        else if startLocation.text != "" || finishLocation.text != "" {
+            clearButton.isEnabled = true
+            goButton.isEnabled = false
+        }
+        else {
+            clearButton.isEnabled = false
+            goButton.isEnabled = false
+        }
+    }
+    
+}
+
+extension ViewController {
+    func setupHideKeyboardOnTap() {
+        self.view.addGestureRecognizer(self.endEditingRecognizer())
+        self.navigationController?.navigationBar.addGestureRecognizer(self.endEditingRecognizer())
+    }
+    
+    private func endEditingRecognizer() -> UIGestureRecognizer {
+        let tap = UITapGestureRecognizer(target: self.view, action: #selector(self.view.endEditing(_:)))
+        tap.cancelsTouchesInView = false
+        return tap
+    }
 }
 
--- maroschupkina1PW7/RoundButton.swift
+++ maroschupkina1PW7/RoundButton.swift
@@ -12,11 +12,11 @@ class RoundButtonView: UIButton {
         self.init()
         backgroundColor = color
         self.setTitle(text, for: .normal)
-        self.layer.cornerRadius = 10
+        self.layer.cornerRadius = 20
         isEnabled = false
         setTitleColor(.gray, for: .disabled)
         translatesAutoresizingMaskIntoConstraints = false
-        widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.height / 12).isActive = true
-        heightAnchor.constraint(equalToConstant: UIScreen.main.bounds.height / 12).isActive = true
+        widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.height / 10).isActive = true
+        heightAnchor.constraint(equalToConstant: UIScreen.main.bounds.height / 20).isActive = true
     }
 }
